7345dbafc41c5b884ad2365c1cc0264d0d96841c
unknown <amir.mohebali76@gmail.com>
2024-02-18 18:16:09 +0330

    adding to studio

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   const projects = await getProjects();
   return (
     <div className="max-w-5xl mx-auto py-20">
-      <h1 className="text-7xl font-extrabold">Hello I'm <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600
+      <h1 className="text-7xl font-extrabold">Hello I&apos;m <span className="bg-gradient-to-r from-orange-400 via-red-500 to-purple-600
       bg-clip-text text-transparent">Kapehe!</span></h1>
       <h2 className="mt-24 font-bold text-gray-700 text-3xl">My Projects</h2>
       <div className="mt-5 grid grid-cols-3 gap-8">
